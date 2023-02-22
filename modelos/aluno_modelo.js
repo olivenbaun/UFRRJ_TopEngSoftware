daa44afbe3534225a59ce8b1a880b829a1cec62a
Caroline Oliveira <carolineoliveira@ufrrj.br>
2023-02-22 16:44:38 -0300

    Commit da branch do professor

---
+++ modelos/aluno_modelo.js
@@ -0,0 +1,80 @@
+// aluno.js componente da camada de modelo da nossa arquitetura MVC
+
+const conexao = require('../BD/conexao')
+
+const fs = require('fs')
+const path = require("path");
+
+
+function lerTodosAlunosBD() {
+    return new Promise(function(resolve, reject) {
+        conexao.query('SELECT * FROM aluno', function (err, result) {
+            if (err) throw reject(err);
+            resolve(result);
+        
+        });
+    });
+
+ 
+}
+
+//Recuperar dados de aluno
+function consultarAlunoBD(matricula) {
+    return new Promise(function(resolve, reject) {
+        conexao.query('SELECT * FROM aluno WHERE matricula ='+matricula, function (err, result) {
+            if (err) throw reject(err);
+            resolve(result);
+        });
+    });
+}
+
+function inserirAlunoBD(aluno) {
+    
+    conexao.connect(function(err) {
+        if (err) {
+          console.error('error connecting: ' + err.stack);
+          return;
+        }
+        console.log('connected as id ' + conexao.threadId);
+    });
+      
+    console.log("recebida requisição inserir aluno no modelo");
+
+    const minhaQuery = 'INSERT INTO aluno (matricula, nome, data_nascimento, email) VALUES ("'+aluno.matricula+'","'+aluno.nome+'","'+aluno.data_nascimento+'","'+aluno.email+'")';
+    
+    console.log("query executada:"+minhaQuery);
+
+    return new Promise(function(resolve, reject) {
+        conexao.query(minhaQuery, function (err, result) {
+            if (err) throw reject(err.message);
+            resolve(result);
+        });
+    });
+}
+
+function deletarAlunoBD(matricula) {
+    return new Promise(function(resolve, reject) {
+        conexao.query('DELETE from aluno WHERE matricula=("'+matricula+'")', function (err, result) {
+            if (err) throw reject(err);
+            resolve(result);
+        });
+    });
+}
+
+function atualizarAlunoBD(aluno){
+    return new Promise(function(resolve, reject) {
+        conexao.query('UPDATE aluno SET nome = "'+aluno.nome+'" WHERE matricula = "'+aluno.matricula+'"', function (err, result) {
+            if (err) throw reject(err);
+            resolve(result);
+        });
+    });
+}
+
+
+module.exports = {
+    lerTodosAlunosBD,
+    consultarAlunoBD,
+    inserirAlunoBD,
+    deletarAlunoBD,
+    atualizarAlunoBD
+}
